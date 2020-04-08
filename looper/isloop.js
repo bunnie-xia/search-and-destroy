df4eaf9a75776b382277704dc255026be10d5dc2
Carmen <xiawenwu22@gmail.com>
2020-04-08 17:50:41 -0400

    isloope

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,7 +2,24 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  if (linkedlist == null) {
+    return false;
+  }
 
+  let tortoise = linkedlist.head;
+  let hare = tortoise;
+
+  // tortoise.next && hare.next would work, but hare is faster anyways
+  while(hare.next) {
+    tortoise = tortoise.next;
+    hare = hare.next.next;
+
+    if (tortoise === hare) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 
