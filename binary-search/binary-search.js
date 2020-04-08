91ae196ba13e66da3cc46272514769edaad2af35
Carmen <xiawenwu22@gmail.com>
2020-04-08 16:10:39 -0400

    draft

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,31 @@
 'use strict';
 
-// Complete this algo
+// Complete this algo //
+// arr = [1,2,3,4,5]; target: 6
+
 const binarySearch = (array, target) => {
-	
+	if (array.length === 1) {
+		return target === array[0];
+	}
+
+	let midIdx = Math.floor(array.length/2);
+	let midpoint = array[midIdx];
+	const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
+	console.log('length:',test1.length);
+
+	console.log(midpoint);
+
+	if (target === midpoint) {
+		return true;
+	} else if (target < midpoint) {
+		let newArr = array.slice(0, midIdx);
+		binarySearch(newArr, target);
+	} else {
+		let newArr = array.slice(midIdx); //[3,4,5] [4,5] [5]
+		console.log('newArr', newArr);
+
+		binarySearch(newArr, target);
+	}
 };
 
 /*
@@ -15,3 +38,8 @@ const binarySearch = (array, target) => {
 */
 
 module.exports = binarySearch
+
+// arr = [1,2,3,4,5];
+// // midIdx = arr.length/2;
+// console.log(arr[2.5]);
+// // console.log(arr[2]);
